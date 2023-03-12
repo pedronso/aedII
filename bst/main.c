166ed016690b509f9f5520777a799adf68fc83d8
pedronso <pedrocaetanoaraujopc@gmail.com>
2023-03-12 00:34:12 -0300

    bst cli improved

--- bst/main.c
+++ bst/main.c
@@ -6,48 +6,57 @@ int main(int argc, char **argv)
 {
   bst_root root;
 
+  int input;
+
   init_bst(&root);
 
-  printf("hello\n");
+  while (1) {
+    printf("1- Inserir elemento\n2- Remover elemento\n3- Consultar elemento\n4- Preorder\n5- Inorder\n6- Posorder\n0- Sair\n");
+    scanf("%d", &input);
 
-  root = bst_insert(root, 10);
-  root = bst_insert(root, 15);
-  root = bst_insert(root, 30);
-  root = bst_insert(root, 5);
-  root = bst_insert(root, 3);
-  root = bst_insert(root, 8);
-  root = bst_insert(root, 1);
-  root = bst_insert(root, 2);
-  root = bst_insert(root, 4);
-
-  
-  bst_pre(root);
-  printf("\n");
-  bst_in(root);
-  printf("\n");
-  bst_pos(root);
-
-  bst_root query = bst_search(root, 15);
-
-  if(query==NULL)
-    printf("valor n existe\n");
-  else
-    printf("valor existe, %d\n", query->data);
-
-  printf("altura: %d\n", bst_height(root));
-  printf("qtd: %d\n", bst_qtd(root));
-  
-  
-  root = bst_remove(root, 5);
-
-  bst_pos(root);
-
-  root = bst_remove(root, 4);
-
-  bst_pos(root);
-
-  root = bst_remove(root, 3);
-
-  bst_pos(root);
+    switch (input) {
+      int data;
+      case 1:
+        printf("Valor a ser inserido: ");
+        scanf("%d", &data);
+        root = bst_insert(root, data);
+        break;
+      case 2:
+        printf("Valor a ser removido: ");
+        scanf("%d", &data);
+        root = bst_remove(root, data);;
+        break;
+      case 3:
+        printf("Valor a ser consultado: ");
+        scanf("%d", &data);
+        bst_root query = bst_search(root, data);
+        if(query==NULL)
+          printf("Valor não existe\n");
+        else
+          printf("Valor existe, %d\n", query->data);
+        printf("\n");
+        break;
+      case 4:
+        bst_pre(root);
+        printf("Altura: %d\n", bst_height(root));
+        printf("Qtd: %d\n", bst_qtd(root));
+        printf("\n");
+        break;
+      case 5:
+        bst_in(root);
+        printf("altura: %d\n", bst_height(root));
+        printf("qtd: %d\n", bst_qtd(root));
+        printf("\n");
+        break;
+      case 6:
+        bst_pos(root);
+        printf("altura: %d\n", bst_height(root));
+        printf("qtd: %d\n", bst_qtd(root));
+        printf("\n");
+        break;
+      case 0:
+        exit(0);
+    }
+  }
 
 }
